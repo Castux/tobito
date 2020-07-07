a56d2971a2a7eab726d5e27e68863cc6495e684f
Noé Falzon <falzon.noe@gmail.com>
2020-07-07 16:37:38 +0300

    Using state scores as AI

--- ai.lua
+++ ai.lua
@@ -38,10 +38,30 @@ local function max_for(values, func)
 	return best
 end
 
+local function state_score(int, player)
+	
+	local s = tobito.int_to_state(int)
+	
+	local sum = 0
+	for cell = 0,tobito.MaxCell do
+		if s[cell] == player then
+			local row = cell // 3
+			if player == tobito.Top then
+				sum = sum + row
+			else
+				sum = sum + (4 - row)
+			end
+		end
+	end
+	
+	return sum
+end
+
 local function decide_state(int, win, top, bottom)
 
 	local state = tobito.int_to_state(int)
 	local player = state.next_player
+	local other = player == tobito.Top and tobito.Bottom or tobito.Top
 	local m = (player == tobito.Top) and 1 or -1
 
 	local win_value = nil
@@ -51,9 +71,15 @@ local function decide_state(int, win, top, bottom)
 		table.insert(acceptable_children, child)
 	end
 
-	local aggressive = max_for(acceptable_children, function(s) return m * (1e6 * win[s] + top[s]) end)
-	local balanced = max_for(acceptable_children, function(s) return m * (1e6 * win[s] + top[s] - bottom[s]) end)
-	local prudent = max_for(acceptable_children, function(s) return m * (1e6 * win[s] - bottom[s]) end)
+	local aggressive = max_for(acceptable_children, function(s)
+		return 1000 * m * win[s] + 10 * state_score(s, player) - 1 * state_score(s, other)
+	end)
+	local balanced = max_for(acceptable_children, function(s)
+		return 1000 * m * win[s] + 10 * state_score(s, player) - 10 * state_score(s, other)
+	end)
+	local prudent = max_for(acceptable_children, function(s)
+		return 1000 * m * win[s] + 1 * state_score(s, player) - 10 * state_score(s, other)
+	end)
 
 	return aggressive, balanced, prudent
 end
